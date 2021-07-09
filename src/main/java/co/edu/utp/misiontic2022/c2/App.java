921e417423f7145b68abab787be080d4201ac7a8
Daniel-Monca <danielmonca77@gmail.com>
2021-07-08 20:11:06 -0500

    Eliminación de comentarios

--- src/main/java/co/edu/utp/misiontic2022/c2/App.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int r=8;
+        int s=5;
+        int su=r+s;
+        System.out.println(su);
+        int res=r-s;
+        System.out.println(res);
     }
 }
